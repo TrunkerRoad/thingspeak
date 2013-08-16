2bd61f00ff050e9dd4624b8e3e9cffc102b82a2a
Daniel Bergey <bergey@alum.mit.edu>
2013-08-15 20:23:24 -0400

    Convert tabs to spaces

--- thingspeak_cpu_loop.py
+++ thingspeak_cpu_loop.py
@@ -5,26 +5,26 @@ import psutil
 import time
 
 def doit():
-	cpu_pc = psutil.cpu_percent()
-	mem_avail_mb = psutil.avail_phymem()/1000000	
-	params = urllib.urlencode({'field1': cpu_pc, 'field2': mem_avail_mb,'key':'YOURKEYHERE'})
-	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-	conn = httplib.HTTPConnection("api.thingspeak.com:80")
-	
-	try:
-		conn.request("POST", "/update", params, headers)
-		response = conn.getresponse()
-		print cpu_pc
-		print mem_avail_mb
-		print strftime("%a, %d %b %Y %H:%M:%S", localtime())
-		print response.status, response.reason
-		data = response.read()
-		conn.close()
-	except:
-		print "connection failed"	
+    cpu_pc = psutil.cpu_percent()
+    mem_avail_mb = psutil.avail_phymem()/1000000
+    params = urllib.urlencode({'field1': cpu_pc, 'field2': mem_avail_mb,'key':'YOURKEYHERE'})
+    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+
+    try:
+        conn.request("POST", "/update", params, headers)
+        response = conn.getresponse()
+        print cpu_pc
+        print mem_avail_mb
+        print strftime("%a, %d %b %Y %H:%M:%S", localtime())
+        print response.status, response.reason
+        data = response.read()
+        conn.close()
+    except:
+        print "connection failed"
 
 #sleep for 16 seconds (api limit of 15 secs)
 if __name__ == "__main__":
-	while True:
-		doit()
-		time.sleep(16) 
+    while True:
+        doit()
+        time.sleep(16)
