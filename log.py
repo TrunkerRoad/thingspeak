96780ea13e7a470c5913536ac133ddbffc4819b4
Daniel Bergey <bergey@alum.mit.edu>
2013-08-21 15:18:28 -0400

    use channel class in log.py example

--- log.py
+++ log.py
@@ -1,23 +1,20 @@
-import httplib, urllib
 from time import localtime, strftime
 # download from http://code.google.com/p/psutil/
 import psutil
 import time
 
+import thingspeak
+
 cpu_pc = psutil.cpu_percent()
-mem_avail_mb = psutil.avail_phymem()/1000000	
-params = urllib.urlencode({'field1': cpu_pc, 'field2': mem_avail_mb,'key':'YOURKEYHERE'})
-headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-conn = httplib.HTTPConnection("api.thingspeak.com:80")
-	
+mem_avail_mb = psutil.avail_phymem()/1000000
+channel = thingspeak.channel('YOURKEYHERE')
+
 try:
-	conn.request("POST", "/update", params, headers)
-	response = conn.getresponse()
-	print cpu_pc
-	print mem_avail_mb
-	print strftime("%a, %d %b %Y %H:%M:%S", localtime())
-	print response.status, response.reason
-	data = response.read()
-	conn.close()
+    response = channel.update([cpu_pc, mem_avail_mb])
+    print cpu_pc
+    print mem_avail_mb
+    print strftime("%a, %d %b %Y %H:%M:%S", localtime())
+    print response.status, response.reason
+    data = response.read()
 except:
-	print "connection failed"
+    print "connection failed"
